f2a0056de0e2ad74eb29c9b6b48c463c5d7d231b
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-06-16 14:57:46 +0600

    Sidebar Icon included

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -3,6 +3,14 @@ import DashboardSharpIcon from "@mui/icons-material/DashboardSharp";
 import GroupOutlinedIcon from "@mui/icons-material/GroupOutlined";
 import ProductionQuantityLimitsOutlinedIcon from "@mui/icons-material/ProductionQuantityLimitsOutlined";
 import AssignmentTurnedInOutlinedIcon from "@mui/icons-material/AssignmentTurnedInOutlined";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import NotificationsActiveOutlinedIcon from "@mui/icons-material/NotificationsActiveOutlined";
+import InsertChartOutlinedTwoToneIcon from "@mui/icons-material/InsertChartOutlinedTwoTone";
+import MonitorHeartOutlinedIcon from "@mui/icons-material/MonitorHeartOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
 
 const Sidebar = () => {
   return (
@@ -26,30 +34,39 @@ const Sidebar = () => {
             <span>Products</span>
           </li>
           <li>
+            <AssignmentTurnedInOutlinedIcon />
             <span>Orders</span>
           </li>
           <li>
+            <LocalShippingIcon />
             <span>Delivery</span>
           </li>
           <li>
-            <span>Stats</span>
-          </li>
-          <li>
+            <NotificationsActiveOutlinedIcon />
             <span>Notification</span>
           </li>
           <li>
+            <InsertChartOutlinedTwoToneIcon />
+            <span>Stats</span>
+          </li>
+          <li>
+            <MonitorHeartOutlinedIcon />
             <span>Health</span>
           </li>
           <li>
-            <span>Logs</span>
-          </li>
-          <li>
+            <SettingsOutlinedIcon />
             <span>Settings</span>
           </li>
           <li>
+            <PsychologyOutlinedIcon />
+            <span>Logs</span>
+          </li>
+          <li>
+            <ExitToAppIcon />
             <span>Logout</span>
           </li>
           <li>
+            <InsertEmoticonIcon />
             <span>Profile</span>
           </li>
         </ul>
