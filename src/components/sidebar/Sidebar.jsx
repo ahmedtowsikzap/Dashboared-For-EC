4398b5416387a6fd52addaebfa2b4be1d695a9bc
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-06-16 14:47:04 +0600

    MUI setup & MUI Icon

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,21 +1,62 @@
-import "./sidebar.scss"
+import "./sidebar.scss";
+import DashboardSharpIcon from "@mui/icons-material/DashboardSharp";
+import GroupOutlinedIcon from "@mui/icons-material/GroupOutlined";
+import ProductionQuantityLimitsOutlinedIcon from "@mui/icons-material/ProductionQuantityLimitsOutlined";
+import AssignmentTurnedInOutlinedIcon from "@mui/icons-material/AssignmentTurnedInOutlined";
 
 const Sidebar = () => {
-    return (
-        <div className="sidebar">
-            <div className="top"><span className="logo">React Admin</span></div>
-            <hr />
-            <div className="center">
-                <ul>
-                    <li><span>Dashboard</span></li>
-                    <li><span>Dashboard</span></li>
-                    <li><span>Dashboard</span></li>
-                    <li><span>Dashboard</span></li>
-                </ul>
-            </div>
-            <div className="bottom">theme</div>
-        </div>
-    );
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <span className="logo">React Admin</span>
+      </div>
+      <hr />
+      <div className="center">
+        <ul>
+          <li>
+            <DashboardSharpIcon />
+            <span>Dashboard</span>
+          </li>
+          <li>
+            <GroupOutlinedIcon />
+            <span>Users</span>
+          </li>
+          <li>
+            <ProductionQuantityLimitsOutlinedIcon />
+            <span>Products</span>
+          </li>
+          <li>
+            <span>Orders</span>
+          </li>
+          <li>
+            <span>Delivery</span>
+          </li>
+          <li>
+            <span>Stats</span>
+          </li>
+          <li>
+            <span>Notification</span>
+          </li>
+          <li>
+            <span>Health</span>
+          </li>
+          <li>
+            <span>Logs</span>
+          </li>
+          <li>
+            <span>Settings</span>
+          </li>
+          <li>
+            <span>Logout</span>
+          </li>
+          <li>
+            <span>Profile</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">theme</div>
+    </div>
+  );
 };
 
 export default Sidebar;
