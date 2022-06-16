5ddd6abe5cbf2473c5a0bf40d596ead73c0f2ac6
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-06-16 15:27:32 +0600

    completed Sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -16,62 +16,70 @@ const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="top">
-        <span className="logo">React Admin</span>
+        <span className="logo">ReactAdmin</span>
       </div>
       <hr />
       <div className="center">
         <ul>
+          <p className="title">MAIN</p>
           <li>
-            <DashboardSharpIcon />
+            <DashboardSharpIcon className="icon" />
             <span>Dashboard</span>
           </li>
+          <p className="title">LISTS</p>
           <li>
-            <GroupOutlinedIcon />
+            <GroupOutlinedIcon className="icon" />
             <span>Users</span>
           </li>
           <li>
-            <ProductionQuantityLimitsOutlinedIcon />
+            <ProductionQuantityLimitsOutlinedIcon className="icon" />
             <span>Products</span>
           </li>
           <li>
-            <AssignmentTurnedInOutlinedIcon />
+            <AssignmentTurnedInOutlinedIcon className="icon" />
             <span>Orders</span>
           </li>
           <li>
-            <LocalShippingIcon />
+            <LocalShippingIcon className="icon" />
             <span>Delivery</span>
           </li>
+          <p className="title">NEEDED</p>
           <li>
-            <NotificationsActiveOutlinedIcon />
+            <NotificationsActiveOutlinedIcon className="icon" />
             <span>Notification</span>
           </li>
           <li>
-            <InsertChartOutlinedTwoToneIcon />
+            <InsertChartOutlinedTwoToneIcon className="icon" />
             <span>Stats</span>
           </li>
+          <p className="title">SERVICES</p>
           <li>
-            <MonitorHeartOutlinedIcon />
+            <MonitorHeartOutlinedIcon className="icon" />
             <span>Health</span>
           </li>
           <li>
-            <SettingsOutlinedIcon />
+            <SettingsOutlinedIcon className="icon" />
             <span>Settings</span>
           </li>
           <li>
-            <PsychologyOutlinedIcon />
+            <PsychologyOutlinedIcon className="icon" />
             <span>Logs</span>
           </li>
           <li>
-            <ExitToAppIcon />
+            <ExitToAppIcon className="icon" />
             <span>Logout</span>
           </li>
+          <p className="title">USER</p>
           <li>
-            <InsertEmoticonIcon />
+            <InsertEmoticonIcon className="icon" />
             <span>Profile</span>
           </li>
         </ul>
       </div>
-      <div className="bottom">theme</div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
     </div>
   );
 };
