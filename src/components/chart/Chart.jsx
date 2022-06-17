c56689897b2d70bad21256b61e5f912344452217
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-06-17 23:33:53 +0600

    chart and featured part folder structure

---
+++ src/components/chart/Chart.jsx
@@ -0,0 +1,11 @@
+import "./chart.scss";
+
+const Chart = () => {
+  return (
+    <div>
+      <h1>chart</h1>
+    </div>
+  );
+};
+
+export default Chart;
---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,11 @@
+import "./feature.scss";
+
+const Featured = () => {
+  return (
+    <div>
+      <h1>Featued Chart js</h1>
+    </div>
+  );
+};
+
+export default Featured;
