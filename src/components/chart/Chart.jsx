653cec4db4cf11456894cf6d5220c9029ba0077c
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-06-18 15:01:14 +0600

    setup rechats js

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,7 +1,11 @@
 import "./chart.scss";
 
 const Chart = () => {
-  return <div className="chart">chart</div>;
+  return (
+    <div className="chart">
+      <h1>chart</h1>
+    </div>
+  );
 };
 
 export default Chart;
