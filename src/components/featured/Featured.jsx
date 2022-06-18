f71235c621d35acc51f07ed78d07f7c1560f432d
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-06-18 14:56:05 +0600

    progressbar design completed

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -2,7 +2,8 @@ import "./featured.scss";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
-
+import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
+import ArrowDropDownOutlinedIcon from "@mui/icons-material/ArrowDropDownOutlined";
 const Featured = () => {
   return (
     <div className="featured">
@@ -19,6 +20,29 @@ const Featured = () => {
         <p className="title">Made today</p>
         <p className="amount">$987</p>
         <p className="desc">Transaction processing</p>
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle">Last Month</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUpOutlinedIcon fontSize="small" />
+              <div className="resultAmount">$9.1K</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last 3 days</div>
+            <div className="itemResult negitive">
+              <ArrowDropDownOutlinedIcon fontSize="small" />
+              <div className="resultAmount">-$3.1K</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUpOutlinedIcon fontSize="small" />
+              <div className="resultAmount">$19.1K</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
