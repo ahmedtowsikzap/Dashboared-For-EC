7ec638e7fc0b7aca0c9e4890d613b6c5a9b74e4a
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-06-19 23:23:26 +0600

    datatable created with  foklder structure

---
+++ src/components/datatable/Datatable.jsx
@@ -0,0 +1,9 @@
+const Datatable = () => {
+  return (
+    <div>
+      <h1>dtaaba</h1>
+    </div>
+  );
+};
+
+export default Datatable;
--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,10 +1,16 @@
-import "./list.scss"
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./list.scss";
 const List = () => {
-    return (
-        <div>
-            list
-        </div>
-    );
+  return (
+    <div className="list">
+      <Sidebar />
+      <div className="listContainer">
+        <Navbar />
+        datatable
+      </div>
+    </div>
+  );
 };
 
 export default List;
