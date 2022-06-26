6eea8aff0eec72bde923aaeae143e0c8730f6bd4
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-06-25 21:24:35 -0700

    view & delete option

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -3,11 +3,27 @@ import { userColumns, userRows } from "../../datatablesource";
 import "./datatable.scss";
 
 const Datatable = () => {
+  const actionColumn = [
+    {
+      field: "action",
+      headerName: "Action",
+      width: 200,
+      renderCell: () => {
+        return (
+          <div className="cellAction">
+            <div className="viewButton">View</div>
+            <div className="deleteButton">Delete</div>
+          </div>
+        );
+      },
+    },
+  ];
+
   return (
     <div className="datatable">
       <DataGrid
         rows={userRows}
-        columns={userColumns}
+        columns={userColumns.concat(actionColumn)}
         pageSize={5}
         rowsPerPageOptions={[5]}
         checkboxSelection
