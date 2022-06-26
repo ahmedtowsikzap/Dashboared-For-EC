f9fa1f111a8177d1b2546bf23389808f5b0f4d39
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-06-25 22:57:13 -0700

    new single page form

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -8,6 +8,49 @@ const New = () => {
       <Sidebar />
       <div className="newContainer">
         <Navbar />
+        <div className="top">
+          <h1>Add new User</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img
+              src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              alt=""
+            />
+          </div>
+          <div className="right">
+            <form>
+              <div className="formInput">
+                <label>Username</label>
+                <input type="text" placeholder="Hon_Joe" />
+              </div>
+              <div className="formInput">
+                <label>Surname</label>
+                <input type="text" placeholder="Hon_Joe" />
+              </div>
+              <div className="formInput">
+                <label>Email</label>
+                <input type="email" placeholder="honjoe@gmail.com" />
+              </div>
+              <div className="formInput">
+                <label>Phone</label>
+                <input type="text" placeholder="+1 122 2453 24 " />
+              </div>
+              <div className="formInput">
+                <label>Password</label>
+                <input type="password" />
+              </div>
+              <div className="formInput">
+                <label>Address</label>
+                <input type="text" />
+              </div>
+              <div className="formInput">
+                <label>Country</label>
+                <input type="text" />
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   );
