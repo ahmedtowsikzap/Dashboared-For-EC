4bc58ef39371d2f88d5f6dff6904668ccbfae255
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-06-25 22:00:32 -0700

    single page with information & edit btn

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,11 +1,32 @@
-import "./single.scss"
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./single.scss";
 
 const Single = () => {
-    return (
-        <div>
-            single
+  return (
+    <div className="single">
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+        <div className="top">
+          <div className="left">
+            <div className="editButton">Edit</div>
+            <h1 className="title">Information</h1>
+            <div className="item">
+              <img
+                src="https://images.pexels.com/photos/2787341/pexels-photo-2787341.jpeg?cs=srgb&dl=pexels-ali-pazani-2787341.jpg&fm=jpg"
+                alt=""
+                className="itemImg"
+              />
+              details
+            </div>
+          </div>
+          <div className="right"></div>
         </div>
-    );
+        <div className="bottom"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Single;
