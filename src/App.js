a854eb383cf756bb02f7a9db94c96f95f1d5bc8e
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-06-25 23:22:02 -0700

    changed some route path to connet to fromsource

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
-import List from "./pages/list/List"
-import Single from "./pages/single/Single"
-import New from "./pages/new/New"
 import {
-  BrowserRouter,
-  Routes,
-  Route,
+  BrowserRouter, Route, Routes
 } from "react-router-dom";
+import { productInputs, userInputs } from "./formSource";
+import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Login from "./pages/login/Login";
+import New from "./pages/new/New";
+import Single from "./pages/single/Single";
 
 
 function App() {
@@ -21,12 +20,12 @@ function App() {
       <Route path="users">
         <Route index element={<List/>}/>
         <Route path=":userId" element={<Single/>}/>
-        <Route path="new" element={<New/>}/>
+        <Route path="new" element={<New inputs = {userInputs} />}/>
       </Route>
       <Route path="products">
         <Route index element={<List/>}/>
         <Route path=":productId" element={<Single/>}/>
-        <Route path="new" element={<New/>}/>
+        <Route path="new" element={<New inputs = {productInputs}/>}/>
       </Route>
     </Route>
     </Routes>
