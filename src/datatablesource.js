24b37ab44e99697f40296492e995e5cf58741eaa
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-06-25 21:07:44 -0700

    changed the links of pictures

--- src/datatablesource.js
+++ src/datatablesource.js
@@ -43,7 +43,7 @@ export const userColumns = [
     {
       id: 1,
       username: "Larry",
-      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      img: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg",
       status: "active",
       email: "larry@gmail.com",
       age: 35,
@@ -51,7 +51,7 @@ export const userColumns = [
     {
       id: 2,
       username: "Joe",
-      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      img: "https://images.pexels.com/photos/1813922/pexels-photo-1813922.jpeg?cs=srgb&dl=pexels-eduardo-dutra-1813922.jpg&fm=jpg",
       email: "joe2w@gmail.com",
       status: "passive",
       age: 42,
@@ -59,7 +59,7 @@ export const userColumns = [
     {
       id: 3,
       username: "Lannister",
-      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      img: "https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?cs=srgb&dl=pexels-spencer-selover-775358.jpg&fm=jpg",
       email: "lannistert@gmail.com",
       status: "pending",
       age: 45,
@@ -75,7 +75,7 @@ export const userColumns = [
     {
       id: 5,
       username: "Benita",
-      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      img: "https://images.pexels.com/photos/5177134/pexels-photo-5177134.jpeg?cs=srgb&dl=pexels-bethany-ferr-5177134.jpg&fm=jpg",
       email: "beniiow@gmail.com",
       status: "passive",
       age: 27,
@@ -83,7 +83,7 @@ export const userColumns = [
     {
       id: 6,
       username: "Floyd may",
-      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      img: "https://images.pexels.com/photos/1561319/pexels-photo-1561319.jpeg?cs=srgb&dl=pexels-leah-kelley-1561319.jpg&fm=jpg",
       email: "floyyyd@gmail.com",
       status: "active",
       age: 19,
@@ -91,7 +91,7 @@ export const userColumns = [
     {
       id: 7,
       username: "Clifford",
-      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      img: "https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?cs=srgb&dl=pexels-creation-hill-1681010.jpg&fm=jpg",
       email: "travis.cliff@gmail.com",
       status: "passive",
       age: 24,
@@ -99,7 +99,7 @@ export const userColumns = [
     {
       id: 8,
       username: "Scoot",
-      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      img: "https://images.pexels.com/photos/5082976/pexels-photo-5082976.jpeg?cs=srgb&dl=pexels-cottonbro-5082976.jpg&fm=jpg",
       email: "dfdow@gmail.com",
       status: "active",
       age: 36,
