0e2d848e8976ecb1b93c1540083821d56e7df3c1
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-06-26 21:04:48 -0700

    context completed

--- src/context/darkModeContext.js
+++ src/context/darkModeContext.js
@@ -1,4 +1,5 @@
-import { createContext } from "react"
+import { createContext, useReducer } from "react";
+import DarkModeReducer from "./darkModeReducer";
 
 const INITIAL_STATE = {
 
@@ -8,5 +9,9 @@ export const DarkModeContext = createContext(INITIAL_STATE)
 
 export const DarkModeContextProvider = ({children}) =>{
 
-    
-}
+    const [state, dispatch] = useReducer(DarkModeReducer, INITIAL_STATE);
+
+    return(
+        <DarkModeContext.Provider value={{darkMode:state.darkMode, dispatch}}>{children}</DarkModeContext.Provider>
+    );
+};
